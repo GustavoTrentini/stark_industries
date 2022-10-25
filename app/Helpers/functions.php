c75f36ffdcc24ac5dfbbfafb2f43e5458fa041fc
gustavo <gtrentini68@gmail.com>
2022-10-25 12:06:02 -0300

    Refatoração de rotina de verificação de fraude

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -1,29 +1,31 @@
 <?php
 
-function sanitizeStringData($str){
+function sanitizeStringData($str)
+{
     $str = preg_replace("/[^a-zA-Z0-9]/", "", $str);
     return (string) $str;
 }
 
-function isFraud($document, $data_nasc){
+function isFraud($document, $data_nasc)
+{
     $isFraud = true;
     $firstNumber = substr($document, 0, 1);
-    $year = (integer) date("Y", strtotime($data_nasc));
+    $year = (int) date("Y", strtotime($data_nasc));
 
-    $x = ['0','1','2','3'];
-    $y = ['4','5','6'];
-    $z = ['7','8','9'];
+    $x = ['0', '1', '2', '3'];
+    $y = ['4', '5', '6'];
+    $z = ['7', '8', '9'];
 
-    if($year <= 1950 && in_array($firstNumber, $x)){
-        $isFraud = false;
-    }
-
-    if($year > 1950 && $year <= 2000 && in_array($firstNumber, $y)){
-        $isFraud = false;
-    }
-
-    if($year >= 2001 && in_array($firstNumber, $z)){
-        $isFraud = false;
+    switch ($year) {
+        case $year <= 1950 && in_array($firstNumber, $x):
+            $isFraud = false;
+            break;
+        case $year > 1950 && $year <= 2000 && in_array($firstNumber, $y):
+            $isFraud = false;
+            break;
+        case $year >= 2001 && in_array($firstNumber, $z):
+            $isFraud = false;
+            break;
     }
 
     return $isFraud;
