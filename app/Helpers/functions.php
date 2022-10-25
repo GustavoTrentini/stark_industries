9f871b3a4ba5ace0f7e67bb031b24937799d333d
gustavo <gtrentini68@gmail.com>
2022-10-25 12:00:21 -0300

    Correção de validação de documento

--- app/Helpers/functions.php
+++ app/Helpers/functions.php
@@ -8,7 +8,7 @@ function sanitizeStringData($str){
 function isFraud($document, $data_nasc){
     $isFraud = true;
     $firstNumber = substr($document, 0, 1);
-    $year = date("Y", strtotime($data_nasc));
+    $year = (integer) date("Y", strtotime($data_nasc));
 
     $x = ['0','1','2','3'];
     $y = ['4','5','6'];
@@ -18,11 +18,11 @@ function isFraud($document, $data_nasc){
         $isFraud = false;
     }
 
-    if(($year > 1950 && $year <= 2000 && in_array($firstNumber, $y)) && $isFraud == true){
+    if($year > 1950 && $year <= 2000 && in_array($firstNumber, $y)){
         $isFraud = false;
     }
 
-    if(($year >= 2001 && in_array($firstNumber, $z)) && $isFraud == true){
+    if($year >= 2001 && in_array($firstNumber, $z)){
         $isFraud = false;
     }
 
